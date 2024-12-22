e76c3a8c3de6109759c1d5b35cd9d0b28c3a9dc6
namandhiman <namandhiman98@gmail.com>
2024-12-22 16:26:49 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Naman Dhiman on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
